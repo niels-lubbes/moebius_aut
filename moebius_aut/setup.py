f29bb91991e81619ed79d1bfbaac73d3f8936554
Niels Lubbes <niels.lubbes@noadds>
2018-05-07 15:13:52 +0200

    Configured for sdist.

--- moebius_aut/setup.py
+++ moebius_aut/setup.py
@@ -7,32 +7,32 @@ https://python-packaging.readthedocs.io/en/latest/minimal.html
 https://pypi.python.org/pypi?%3Aaction=list_classifiers
 '''
 
-
 from setuptools import setup
 
 
-setup( name = 'moebius_aut',
-       version = '0',
-       description = 'Moebius automorphisms of surfaces',
-       classifiers = [
-           'Development Status :: 3 - Alpha',
-           'License :: OSI Approved :: MIT License',
-           'Programming Language :: Python :: 2',
-           'Programming Language :: Python :: 3',
-           'Topic :: Scientific/Engineering :: Mathematics',
-           ],
-      keywords = 'surfaces automorphisms circles',
-      url = 'http://github.com/niels-lubbes/moebius_aut',
-      author = 'Niels Lubbes',
-      license = 'MIT',
-      package_dir = {'': 'src'},
-      packages = ['moebius_aut'],
-      test_suite = 'nose.collector',
-      tests_require = ['nose'],
-      entry_points = {
-          'console_scripts': ['run-moebius_aut=moebius_aut.__main__:main'],
-      },
-      include_package_data = True,
-      zip_safe = False )
-
+setup( 
+    name = 'moebius_aut',
+    version = '0',
+    description = 'Computing Moebius automorphisms of surfaces',
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        ],
+    keywords = 'surfaces automorphisms circles',
+    url = 'http://github.com/niels-lubbes/moebius_aut',
+    author = 'Niels Lubbes',
+    license = 'MIT',
+    package_dir = {'moebius_aut': 'src/moebius_aut'},
+    packages = ['moebius_aut'],
+    package_data = {'moebius_aut': ['ma_tools.sobj']},
+    test_suite = 'nose.collector',
+    tests_require = ['nose'],
+    entry_points = {
+        'console_scripts': ['run-moebius_aut=moebius_aut.__main__:main'],
+    },
+    zip_safe = False
+    )
 
